711dfcbd3f1b644e8aa6488667cb668b6b2cd812
Daniyal Durrani <daniyal.durrani@alphamudigital.com>
2026-03-21 14:47:44 +0500

    feat: Updated main.rs and implmented functionality that lets users provide arguments when running program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::{env, process};
+
+#[derive(Debug)]
+struct Config {
+    query: String,
+    file_name: String
+}
+
+impl Config {
+    fn new(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("required query not found")
+        };
+        let file_name = match args.next() {
+            Some(arg) => arg,
+            None => return Err("required file_name not found")
+        };
+
+        Ok(Config { query, file_name })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli_args = env::args();
+    let config = Config::new(cli_args).unwrap_or_else(|error| {
+        eprintln!("{error}");
+        process::exit(1);
+    });
+
+    println!("{config:?}")
 }
